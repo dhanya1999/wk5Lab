3f6960079cfb5bbd0f202a560e6078ecdd90ccac
dhanya1999 <54580195+dhanya1999@users.noreply.github.com>
2019-08-27 22:56:44 +1000

    initial commit

--- wk5-lab.js
+++ wk5-lab.js
@@ -6,10 +6,16 @@ app.set("viewengine");
 
 let bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
 
 app.use(express.static('images'));
 let viewsPath = __dirname+'/views/';
+
+/* 
+          GET Requests
+  */
 
 app.get('/', function(req,res){    
     res.sendFile(viewsPath+"index.html");
@@ -19,16 +25,20 @@ app.get('/addNewtask', function(req,res){
     res.sendFile(viewsPath+"addNew.html");
 });
 
-app.post('/newTask', function(req,res){
-    db.push(req.body);
-    res.sendFile(viewsPath+"addNew.html");
-})
-
 app.get('/listAll', function(req,res){    
     res.render(viewsPath + "listAllTasks.html",{
         tasks: db
     });
+/* 
+          POST Requests
+  */
+app.post('/newTask', function(req,res){
+    db.push(req.body);
+    res.sendFile(viewsPath+"addNew.html");
+});
+
 });
 
 
 app.listen(8080);
+console.log("Listening on port 8080");
